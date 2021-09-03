e71b8304674b509ffd392ca246673e822084ddff
liuseki <ld@meng2333.com>
2021-09-03 17:55:49 +0800

    view: user_list: 完善类型注释

--- app/user/views.py
+++ app/user/views.py
@@ -1,10 +1,17 @@
 # coding: utf-8
 
+from __future__ import annotations
 import time
+from typing import Any
 
 from sanic import Request, json, HTTPResponse
+from sqlalchemy.engine.row import Row
+from sqlalchemy.engine.result import ChunkedIteratorResult
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from sqlalchemy import desc
+from sqlalchemy.sql.selectable import Select
+
 from app.models import User
 from libs.logger import LoggerProxy
 from libs.sqlalchemy.result import result_format
@@ -14,15 +21,15 @@ logger: LoggerProxy = LoggerProxy(__name__)
 
 async def user_list(request: Request) -> HTTPResponse:
     """用户列表"""
-    page = 1
-    per_page = 5
-    db_session = request.ctx.db_session
-    query = select(User.id, User.name).select_from(User).order_by(desc(User.ctime), desc(User.id)). \
+    page: int = 1
+    per_page: int = 5
+    db_session: AsyncSession = request.ctx.db_session
+    query: Select = select(User.id, User.name).select_from(User).order_by(desc(User.ctime), desc(User.id)). \
         offset((page - 1) * per_page).limit(per_page)
     async with db_session.begin():
-        cur_result = await db_session.execute(query)
-    result = cur_result.fetchall()
-    data = result_format(result)
+        cur_result: ChunkedIteratorResult = await db_session.execute(query)
+    result: list[Row] = cur_result.fetchall()
+    data: list[dict[str, Any]] = result_format(result)
     # 测试慢日志中间件
     # time.sleep(1)
     return json({'data': data})
