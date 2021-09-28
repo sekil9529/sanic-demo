1bc6935655be68cd072b110fd816615b6e70642b
liuseki <ld@meng2333.com>
2021-09-28 15:04:14 +0800

    settings.production: pool_size=20

--- settings/production.py
+++ settings/production.py
@@ -20,7 +20,7 @@ class Settings(BaseSettings):
     SQLALCHEMY_DATABASE_URL: str = f"mysql+aiomysql://{CONFIG_INFO['db']['user']}:{CONFIG_INFO['db']['password']}" \
                                    f"@{CONFIG_INFO['db']['host']}/{CONFIG_INFO['db']['database']}?charset=utf8mb4"
     SQLALCHEMY_ENGINE_OPTIONS: Dict[str, Any] = {
-        'pool_size': 10,
+        'pool_size': 20,
         'max_overflow': 0,
         'pool_recycle': 60 * 60 * 2,
         # 'pool_reset_on_return': None,  # 放回时执行的操作，默认执行 'rollback'
